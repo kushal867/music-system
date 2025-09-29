1f3187223ecb25626350d840f80ceff89f8f1a3a
Kushal <your_email@example.com>
2025-09-30 00:13:51 +0545

    ADD the urls for the login

--- music/urls.py
+++ music/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('favorites/', views.my_favorites, name='favorites'),
+    path('favorites/', views.my_favorites, name='favorites'),  # ✅ changed name here
     path('add-favorite/<int:song_id>/', views.add_favorite, name='add_favorite'),
+    path('remove-favorite/<int:song_id>/', views.remove_favorite, name='remove_favorite'),
+    path('logout/', views.logout_view, name='logout'),
 ]
